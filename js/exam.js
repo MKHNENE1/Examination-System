4f4ab3240bc3f62a98405532b3c50cb9923cb545
MKHNENE1 <mohamedhalabya11@gmail.com>
2024-07-07 19:09:27 +0200

    updates

--- js/exam.js
+++ js/exam.js
@@ -7,65 +7,64 @@ window.onload = function () {
   }
 };
 
-
 const quizData = [
   {
-      question: "Which islands are coral-covered in the Bay of Bengal?",
-      a: "Maldives",
-      b: "Andaman and Nicobar",
-      c: "Lakshadweep",
-      d: "Fiji",
-      correct: "b"
+    question: "Which islands are coral-covered in the Bay of Bengal?",
+    a: "Maldives",
+    b: "Andaman and Nicobar",
+    c: "Lakshadweep",
+    d: "Fiji",
+    correct: "b",
   },
   // Add more questions as objects here
 ];
 
-const quiz = document.getElementById('quiz');
-const results = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+const quiz = document.getElementById("quiz");
+const results = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 
-function loadQuiz() {
-  const output = [];
-  quizData.forEach((currentQuestion, questionNumber) => {
-      const answers = [];
-      for (letter in currentQuestion) {
-          if (letter !== 'question' && letter !== 'correct') {
-              answers.push(
-                  `<label>
-                      <input type="radio" name="question${questionNumber}" value="${letter}">
-                      ${currentQuestion[letter]}
-                  </label>`
-              );
-          }
-      }
-      output.push(
-          `<div class="question">${currentQuestion.question}</div>
-          <div class="answers">${answers.join('')}</div>`
-      );
-  });
-  quiz.innerHTML = output.join('');
-}
+// function loadQuiz() {
+//   const output = [];
+//   quizData.forEach((currentQuestion, questionNumber) => {
+//     const answers = [];
+//     for (letter in currentQuestion) {
+//       if (letter !== "question" && letter !== "correct") {
+//         answers.push(
+//           `<label>
+//                       <input type="radio" name="question${questionNumber}" value="${letter}">
+//                       ${currentQuestion[letter]}
+//                   </label>`
+//         );
+//       }
+//     }
+//     output.push(
+//       `<div class="question">${currentQuestion.question}</div>
+//           <div class="answers">${answers.join("")}</div>`
+//     );
+//   });
+//   quiz.innerHTML = output.join("");
+// }
 
 function showResults() {
-  const answerContainers = quiz.querySelectorAll('.answers');
+  const answerContainers = quiz.querySelectorAll(".answers");
   let numCorrect = 0;
 
   quizData.forEach((currentQuestion, questionNumber) => {
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      if (userAnswer === currentQuestion.correct) {
-          numCorrect++;
-          answerContainers[questionNumber].style.color = 'green';
-      } else {
-          answerContainers[questionNumber].style.color = 'red';
-      }
+    if (userAnswer === currentQuestion.correct) {
+      numCorrect++;
+      answerContainers[questionNumber].style.color = "green";
+    } else {
+      answerContainers[questionNumber].style.color = "red";
+    }
   });
 
   results.innerHTML = `${numCorrect} out of ${quizData.length}`;
 }
 
-submitButton.addEventListener('click', showResults);
+// submitButton.addEventListener('click', showResults);
 
-loadQuiz();
+// loadQuiz();
